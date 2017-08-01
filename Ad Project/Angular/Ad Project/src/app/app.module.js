f1b37b10b81412556c10539e45ea6ac3cbb637f7
Aditya <aditya.khare@xoriant.com>
2017-08-01 18:04:41 +0530

    local

---
+++ Ad Project/Angular/Ad Project/src/app/app.module.js
@@ -0,0 +1,28 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var app_component_1 = require("./app.component");
+var http_1 = require("@angular/http");
+var navbar_component_1 = require("./components/navbar-component/navbar.component");
+var footer_component_1 = require("./components/footer-component/footer.component");
+var login_component_1 = require("./components/login-component/login.component");
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [platform_browser_1.BrowserModule, http_1.HttpModule],
+        declarations: [app_component_1.AppComponent, navbar_component_1.NavBarComponentClass, footer_component_1.FooterComponentClass, login_component_1.LoginComponentClass],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ Ad Project/Angular/Ad Project/src/app/components/login-component/login.component.js
@@ -0,0 +1,49 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var user_service_1 = require("../../services/UserService/user.service");
+var LoginComponentClass = (function () {
+    function LoginComponentClass(registerService) {
+        this.registerService = registerService;
+    }
+    LoginComponentClass.prototype.sendToUserServiceRegister = function (fname, lname, uname, pwd, email, phone) {
+        var msg;
+        var user = {
+            firstName: fname,
+            lastName: lname,
+            userName: uname,
+            password: pwd,
+            email: email,
+            phone: phone
+        };
+        this.registerService.RegisterUser(user).subscribe(function (response) { console.log("Server says", response); }, function (error) { alert("User Already Exist!"); });
+    };
+    LoginComponentClass.prototype.sendToUserServiceLogin = function (username, password) {
+        if (username === void 0) { username = null; }
+        if (password === void 0) { password = null; }
+        var userlogin = {
+            userName: username,
+            password: password
+        };
+        this.registerService.LoginUser(userlogin).subscribe(function (response) { console.log(response); }, function (error) { alert("Wrong Credentials"); });
+    };
+    return LoginComponentClass;
+}());
+LoginComponentClass = __decorate([
+    core_1.Component({
+        selector: 'login',
+        templateUrl: "./login-html.html",
+        providers: [user_service_1.UserService]
+    }),
+    __metadata("design:paramtypes", [user_service_1.UserService])
+], LoginComponentClass);
+exports.LoginComponentClass = LoginComponentClass;
+//# sourceMappingURL=login.component.js.map
